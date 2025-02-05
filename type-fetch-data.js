8e54637b1a47b6ab3e90d3d1cbaf903d57b61670
Surha <surha_dag@mail.ru>
2025-02-05 22:28:00 +0300

    дополнил структуру типов базы данных

--- type-fetch-data.js
+++ type-fetch-data.js
@@ -1,50 +1,6 @@
 // тут тыпы загружаемых с 1с данных
 
-// поставщики
-// адрес  /suppliers/
-const suppliers = [
-  {
-    'guid': '4c8d40f8-4c4b-4d55-a60b-4b92a0bc5a1e', 
-    'name': 'Поставщик 1', 
-    'code_1c': '1C001'
-  }
-]
-
-// номенклатуры
-// адрес  /nomenclatures/
-const nomenclatures = [
-  {
-    'guid': '5b8f40f8-4c4b-4d55-a60b-4b92a0bc5a1f', 
-    'name': 'Товар 1', 
-    'code_1c': 'T001', 
-    'supplier_guid': '4c8d40f8-4c4b-4d55-a60b-4b92a0bc5a1e'
-  }
-]
-
-// единицы измерения
-// адрес  /units/
-const units = [
-  {
-    'guid': '7d8f40f8-4c4b-4d55-a60b-4b92a0bc5a1g', 
-    'name': 'шт', 
-    'code_1c': 'U001', 
-    'coefficient': 1.0, 
-    'is_base': True, 
-    'nomenclature_guid': '5b8f40f8-4c4b-4d55-a60b-4b92a0bc5a1f'
-  }
-]
-
-// партнёры
-// адрес  /partners/
-const partners = [
-  {
-    'guid': '3c7f40f8-4c4b-4d55-a60b-4b92a0bc5a1d', 
-    'name': 'Партнёр 1', 
-    'code_1c': 'P001'
-  }
-]
-
-// контрагенты
+// контрагенты (клиенты)
 // адрес  /contractors/
 const contractors = [
   {
@@ -59,14 +15,13 @@ const contractors = [
   }
 ]
 
-
-// даркстор
-// адрес  /dark_stores/
-const dark_stores = [
+// номенклатуры
+// адрес  /nomenclatures/
+const nomenclatures = [
   {
-    'guid': '8f8f40f8-4c4b-4d55-a60b-4b92a0bc5a1i', 
-    'name': 'Даркстор 1', 
-    'code_1c': 'DS001', 
+    'guid': '5b8f40f8-4c4b-4d55-a60b-4b92a0bc5a1f', 
+    'name': 'Товар 1', 
+    'code_1c': 'T001', 
     'supplier_guid': '4c8d40f8-4c4b-4d55-a60b-4b92a0bc5a1e'
   }
 ]
@@ -89,4 +44,83 @@ const add_price_type = [
     'price_type_guid': '9f8f40f8-4c4b-4d55-a60b-4b92a0bc5a1j'
   }
 ]
+
+// единицы измерения
+// адрес  /units/
+const units = [
+  {
+    'guid': '7d8f40f8-4c4b-4d55-a60b-4b92a0bc5a1g', 
+    'name': 'шт', 
+    'code_1c': 'U001', 
+    'coefficient': 1.0, 
+    'is_base': True, 
+    'nomenclature_guid': '5b8f40f8-4c4b-4d55-a60b-4b92a0bc5a1f'
+  }
+]
+
+// заказы
+// адрес  /orders/
+const orders = [
+  {
+    "id": 1,
+    "order_number": 1001,
+    "order_date": "2024-11-27",
+    "delivery_date": "2024-11-30",
+    "contractor": "ООО Ромашка",
+    "comment": "Срочная доставка",
+    "delivery_address": "г. Москва, ул. Тверская, д. 10",
+    "total_sum": 12000.50,
+    "items": [
+      {
+        "line_number": 1,
+        "guid_product": "c44b57af-2345-4f1b-b817-61de8965b91c",
+        "guid_unit": "a12b57af-1234-4f1b-b817-61de1234b91a",
+        "quantity": 10,
+        "price": 500.25,
+        "total": 5002.50
+      },
+      {
+        "line_number": 2,
+        "guid_product": "d24c97af-5678-4f1b-b817-61de8965c92b",
+        "guid_unit": "b78c97af-5678-4f1b-b817-61de5678c92b",
+        "quantity": 15,
+        "price": 466.50,
+        "total": 6997.50
+      }
+    ]
+  },
+]
+
+
+// партнёры
+// адрес  /partners/
+// const partners = [
+//   {
+//     'guid': '3c7f40f8-4c4b-4d55-a60b-4b92a0bc5a1d', 
+//     'name': 'Партнёр 1', 
+//     'code_1c': 'P001'
+//   }
+// ]
+
+// поставщики
+// адрес  /suppliers/
+// const suppliers = [
+//   {
+//     'guid': '4c8d40f8-4c4b-4d55-a60b-4b92a0bc5a1e', 
+//     'name': 'Поставщик 1', 
+//     'code_1c': '1C001'
+//   }
+// ]
+
+// даркстор (склады)
+// адрес  /dark_stores/
+// const dark_stores = [
+//   {
+//     'guid': '8f8f40f8-4c4b-4d55-a60b-4b92a0bc5a1i', 
+//     'name': 'Даркстор 1', 
+//     'code_1c': 'DS001', 
+//     'supplier_guid': '4c8d40f8-4c4b-4d55-a60b-4b92a0bc5a1e'
+//   }
+// ]
+
 
