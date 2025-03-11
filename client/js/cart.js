fa9dd55873c81f244ec9e6ec38ac151c6b4e303f
Surha <surha_dag@mail.ru>
2025-03-12 00:24:13 +0300

    корзина доработки

--- client/js/cart.js
+++ client/js/cart.js
@@ -5,18 +5,48 @@ import { get_suppliers } from "./API/suppliers/index.js"
   const section = document.querySelector('#cart')
   const list = section.querySelector('#product-list')
   const total_sum = section.querySelector('#total-sum')
+  const discount_el = section.querySelector('#discount')
+  const bonus_el = section.querySelector('#bonus')
+  const final_sum_el = section.querySelector('#final-sum')
+  const count_prod_el = section.querySelector('.cart__count-product')
+  const btn_offer = section.querySelector('#btn-offer')
+  
+  
 
   let nomenclatures = await get_nomenclatures()
   let suppliers = await get_suppliers()
   
   let products_id = []
   products_id = get_from_LS()
+  let is_bonus = true
+  let bonus = 100
+  let is_discount = true
   render_list()
   calc_total_sum()
+  count_prod()
   section.addEventListener('click', del_prod)
   section.addEventListener('click', choice_minus)
   section.addEventListener('click', choice_plus)
+  section.addEventListener('click', change_radio)
+  section.addEventListener('click', change_discount)
 
+  function change_radio(e) {
+    if(!e.target.closest('input[name=bonus]')) return
+    if(e.target.value == 'true') is_bonus = true
+    if(e.target.value == 'false') is_bonus = false
+    calc_total_sum()
+  }
+  function change_discount(e) {
+    if(!e.target.closest('input[name=discount]')) return
+    if(e.target.value == 'true') is_discount = true
+    if(e.target.value == 'false') is_discount = false
+    calc_total_sum()
+  }
+  function count_prod() {
+    let list = section.querySelectorAll('.cart__list')
+    count_prod_el.textContent = list.length + ' товара'
+    if(list.length == 0) btn_offer.classList.remove('gradient')
+  }
   function choice_plus(e) {
     if(!e.target.closest('.cart__choice-plus')) return
     let prod = e.target.closest('.cart__list')
@@ -50,12 +80,29 @@ import { get_suppliers } from "./API/suppliers/index.js"
   }
   function calc_total_sum() {
     let sum = 0
+    let final_sum = 0
     let list = section.querySelectorAll('#sum-prod')
     for(let el of list) {
       let sum_prod = parseInt(el.textContent)
       sum += sum_prod
     }
-    total_sum.textContent = sum + ' руб.'
+    total_sum.textContent = sum + ' руб'
+    final_sum = sum
+    if(is_bonus) {
+      bonus_el.textContent = bonus + ' руб'
+      final_sum -= bonus
+    } else {
+      bonus_el.textContent = '0 руб'
+    }
+    if(is_discount) {
+      let discount = Math.floor(sum * 0.03)
+      discount_el.textContent = discount + ' руб'
+      final_sum -= discount
+    } else {
+      discount_el.textContent = '0 руб'
+    }
+    if(final_sum < 0) final_sum = 0
+    final_sum_el.textContent = final_sum + ' руб'
   }
   function del_prod(e) {
     if(!e.target.closest('#del-icon')) return
@@ -64,6 +111,7 @@ import { get_suppliers } from "./API/suppliers/index.js"
     products_id = products_id.filter(el => el != id)
     render_list()
     calc_total_sum()
+    count_prod()
   }
   function render_list() {
     list.innerHTML = ''
